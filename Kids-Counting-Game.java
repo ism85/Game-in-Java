3b5166bdca895c68b0e30307919e94632ba32d1d
Ismail <31880917+ism85@users.noreply.github.com>
2021-03-29 15:53:14 +0200

    Add files via upload

---
+++ Kids-Counting-Game.java
@@ -0,0 +1,34 @@
+public class KidsCountingGame{
+
+ 
+    public static void main(String[] args) {
+
+        System.out.println("Please enter your number from 1 to 18");
+        
+        for (int number=1; number<=18; number++){
+            if (number == 3) {
+                System.out.println("ka");
+              }
+              else if (number == 5){
+                  System.out.println("ching!");
+              }
+              else if (number == 9){
+                System.out.println("ka");
+              }
+              else if (number == 10){
+                System.out.println("ching!");
+              }
+              else if (number == 12){
+                System.out.println("ka");
+              }
+              else if (number == 15){
+                System.out.println("ka-ching!");
+              }
+              else if (number == 18){
+                System.out.println("ka");
+              }
+              
+        }
+    }
+
+}
